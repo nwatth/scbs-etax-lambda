6c68f220e006c56037366620db09f965a1ff61c0
Chayakorn Joe <chayakorn.taktuan@scb.co.th>
2021-05-14 10:23:34 +0700

    Update pipeline

---
+++ SendEmail/jenkins/deploy-uat.groovy
@@ -0,0 +1,14 @@
+#!groovy
+
+library 'etax-jenkins-libraries'
+deployLamda(
+    emailReceiver: 'chayakorn.taktuan@scb.co.th',
+    environment: 'uat',
+    fileName: 'SendEmail.zip',
+    aws_profile: 'etax-uat',
+    aws_region: 'ap-southeast-1',
+    aws_bucket: 's3-sourcecode-scbs-nonprod',
+    aws_bucket_path: 'deployment-package',
+    aws_lambda_func_name: 'scbs-lambda-etax-send-email-uat',
+)
+
